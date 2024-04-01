1d0a2c4ba0498140b049ff3001dba2e98907d7f1
Nithishkumar05A <165248246+Nithishkumar05A@users.noreply.github.com>
2024-04-01 14:12:01 +0530

    Add files via upload

---
+++ NITHISH KUMAR python code.py
@@ -0,0 +1,22 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/STUDENT/AppData/Local/Programs/Python/Python312/image code.py
+Traceback (most recent call last):
+  File "C:/Users/STUDENT/AppData/Local/Programs/Python/Python312/image code.py", line 5, in <module>
+    "execution_count": null,
+NameError: name 'null' is not defined
+>>> 
+= RESTART: C:/Users/STUDENT/AppData/Local/Programs/Python/Python312/image code.py
+Traceback (most recent call last):
+  File "C:/Users/STUDENT/AppData/Local/Programs/Python/Python312/image code.py", line 26, in <module>
+    "execution_count": null,
+NameError: name 'null' is not defined
+>>> 
+= RESTART: C:/Users/STUDENT/AppData/Local/Programs/Python/Python312/image code.py
+Traceback (most recent call last):
+  File "C:/Users/STUDENT/AppData/Local/Programs/Python/Python312/image code.py", line 97, in <module>
+    "id": tutorial-anderson,
+NameError: name 'tutorial' is not defined
+>>> 
+= RESTART: C:/Users/STUDENT/AppData/Local/Programs/Python/Python312/image code.py
